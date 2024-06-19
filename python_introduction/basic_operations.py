0ef871a17cc710ee904813d8551837cba50382c0
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-06-19 23:36:07 +0000

    add a new file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+# Addition
+sum = number1 + number2
+# Substraction
+difference = number1 - number2
+# Multiplication
+multiplication = number1 * number2
+
+print ("Addition of", number1 ,"and", number2, "is", sum)
+print("Substraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
