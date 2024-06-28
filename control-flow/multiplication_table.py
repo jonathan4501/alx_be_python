6ab3e1a4400ccfec0dbf27885be787a86c3b2cbf
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-06-28 16:52:36 +0000

    add new file

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,6 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     product = i * number
-    print(f"{number} x {i} = {product}")
+    print(f"{number} x {i} = {product}", end="\t")
 
 
